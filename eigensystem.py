682f9c6582ffcfe77f145f5877e13195e276d777
Sherwood Richers <srichers@caltech.edu>
2021-06-07 10:52:19 -0700

    added license header to python script

--- eigensystem.py
+++ eigensystem.py
@@ -1,3 +1,21 @@
+# Copyright (C) 2021 Sherwood Richers
+# This file is part of neutrino_linear_stability <https://github.com/srichers/neutrino_linear_stability>.
+#
+# neutrino_linear_stability is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# neutrino_linear_stability is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with neutrino_linear_stability.  If not, see <http://www.gnu.org/licenses/>.
+
+#======================================================================#
+
 # set "target_resolution" to the target resolution before
 # I call the script from two directories up, so for me it is:
 # python ../../refine_global_piecewise_constant.py
